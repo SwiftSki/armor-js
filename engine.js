6870d7f27c57f8804084c85991e1933a21a71fc2
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-10-19 08:43:19 -0600

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,3 +1,56 @@
 class Engine {
-	
+	constructor(World, Render, options){
+    this.world = World;
+    this.render = Render;
+
+    //this takes the options and applies them if needed
+		let prop;
+		for (prop in this) {
+			if (Object.prototype.hasOwnProperty.call(this, prop)) {
+				this[prop] = this[prop];
+			}
+		}
+		for (prop in options) {
+			if (Object.prototype.hasOwnProperty.call(options, prop)) {
+				this[prop] = options[prop];
+			}
+		}
+  }
+  tick(callback){ //moves the engine and renderer forward
+    this.render.draw();
+    this.world.entities.map(obj => {
+      if(!obj.isStatic){
+        //gravity
+        obj.force.y += 10;
+        
+        //air resistance (probably not the right math)
+        obj.force.y -= ((world.airResistance + 1) * obj.velocity.y) - obj.mass;
+        
+        //normal force (gotta figure out hitboxes)
+
+        //world limits (should remove object)
+        if(obj.x > this.world.bounds.max.x) {
+          this.world.remove(obj);
+        } else {
+          if(obj.x < this.world.bounds.min.x) {
+            this.world.remove(obj);
+          }
+        }
+
+        //move stuff
+        
+      }
+    });
+
+    if({}.toString.call(callback) === '[object Function]'){callback();}
+  }
+  start(ms, callback){ //starts engine ticking and rendering
+    let loop = () => {
+      this.tick(callback);
+    }
+    this.runner = setInterval(loop, ms);
+  }
+	stop(){
+    clearInterval(this.runner);
+	}
 }
